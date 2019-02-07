5763fa0c611182a38ee43c7c253abc0a1df4d6d1
mzandvliet <martijn@ramjetanvil.com>
2019-02-07 17:43:31 +0100

    Implemented multi-resolution Haar transform recursively using NativeSlice. It's neat! Make me want to write this in Rust later.

--- Assets/Scripts/HaarTransform.cs
+++ Assets/Scripts/HaarTransform.cs
@@ -5,27 +5,53 @@ using Unity.Collections;
 using Unity.Mathematics;
 using Rng = Unity.Mathematics.Random;
 
+/*
+    Implementation Notes
+
+    - Slice notation is very welcome! Writing the recursive logic
+    for the multi-resolution Haar transform becomes very simple.
+    This is how Rust does it, except that Run might have additional
+    tricks available?
+    - Going through floating-point paths to calculate logarithms that
+    we know will range into the natural numbers is weird.
+    - In Rust, instead of writing our Transform functions with type
+    signature Slice<float>, we can write: Slice<TNum>, where TNum
+    is any type that supports addition, subtraction, and division-by-sqrt(2)
+        - Now, dividing by an irrational isn't great for fixed point, but TNum
+        could totally be a ring of fixed point types. That sqrt(2) though...
+
+ */
+
 public class HaarTransform : MonoBehaviour {
     private NativeArray<float> _input;
-    private NativeArray<float> _output;
+
+    private NativeArray<float> _bufferA;
+    private NativeArray<float> _bufferB;
 
     private const float sqrt2 = 1.41421356237f;
 
     private void Awake() {
-        _input = new NativeArray<float>(64, Allocator.Persistent);
-        _output = new NativeArray<float>(64, Allocator.Persistent);
+        const int signalLength = 64;
+        _input = new NativeArray<float>(signalLength, Allocator.Persistent);
+        _bufferA = new NativeArray<float>(signalLength, Allocator.Persistent);
+        _bufferB = new NativeArray<float>(signalLength, Allocator.Persistent);
+
+        int maxDepth = MaxTransformDepth(signalLength);
+        Debug.LogFormat("Signal length: {0}, Max depth: {1}", signalLength, maxDepth);
 
         Generate(_input);
-        Transform(_input, _output);
+        Copy(_input, _bufferA);
+        var output = Transform(_bufferA, _bufferB, maxDepth);
 
-        double eInput = Energy(_input);
-        double eOutput = Energy(_output);
+        double eInput = SumOfSqrs(_input);
+        double eOutput = SumOfSqrs(output);
         Debug.LogFormat("Energy difference: {0}", eInput - eOutput);
     }
 
     private void OnDestroy() {
         _input.Dispose();
-        _output.Dispose();
+        _bufferA.Dispose();
+        _bufferB.Dispose();
     }
 
     private static void Generate(NativeArray<float> input) {
@@ -36,8 +62,46 @@ public class HaarTransform : MonoBehaviour {
         }
     }
 
+    private static void Copy(NativeArray<float> a, NativeArray<float> b) {
+        for (int i = 0; i < a.Length; i++) {
+            b[i] = a[i];
+        }
+    }
 
-    private static void Transform(NativeArray<float> input, NativeArray<float> output) {
+    /// <summary>
+    /// Recursively applies Haar transform up to given depth of iterations.
+    /// </summary>
+    /// <param name="a">Buffer containing input data</param>
+    /// <param name="b">Additional buffer used in computation</param>
+    /// <param name="depth">Pointer to buffer containing output data, which is either a or b.</param>
+    /// <returns></returns>
+    private static NativeSlice<float> Transform(NativeSlice<float> a, NativeSlice<float> b, int depth) {
+        if (depth > MaxTransformDepth(a.Length)) {
+            throw new System.ArgumentException(string.Format("Depth {0} cannot exceed maximum depth of {1} for input with length {2}", depth, MaxTransformDepth(a.Length), a.Length));
+        }
+
+        for (int i = 0; i < depth; i++) {
+            // transform on slices recursively, pingponging between the arrays
+            // each level transforms the previous trend signal, which is the
+            // left half of the input buffer
+            int extents = a.Length / ((int)Mathf.Pow(2, i));
+            Transform(a.Slice(0, extents), b.Slice(0, extents));
+
+            // swap the buffers
+            var temp = a;
+            a = b;
+            b = temp;
+        }
+
+        return a;
+    }
+
+    /// <summary>
+    /// Applies one level of the Haar transform
+    /// </summary>
+    /// <param name="input">Input buffer</param>
+    /// <param name="output">Output buffer containing [trend,fluctuations]</param>
+    private static void Transform(NativeSlice<float> input, NativeSlice<float> output) {
         var trend = output.Slice(0, input.Length/2);
         var fluct = output.Slice(input.Length/2, input.Length/2);
 
@@ -47,11 +111,15 @@ public class HaarTransform : MonoBehaviour {
         }
     }
 
-    private static double Energy(NativeArray<float> signal) {
+    private static double SumOfSqrs(NativeSlice<float> signal) {
         double e = 0.0;
         for (int i = 0; i < signal.Length; i++) {
             e += signal[i] * signal[i];
         }
         return e;
+    }
+
+    private static int MaxTransformDepth(int signalLength) {
+        return (int)math.round(math.log2(signalLength)) - 1;
     }
 }
